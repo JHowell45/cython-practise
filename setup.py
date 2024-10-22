287576e288c82ef709263b766189d9f3cb3e25a5
= <jhowell45@googlemail.com>
2024-10-23 00:34:55 +0100

    Updated how the extensions are built.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import Cython.Compiler.Options
+from Cython.Build import cythonize
 from setuptools import Extension, setup
+
+Cython.Compiler.Options.annotate = True
+
+extensions = [
+    Extension("fibonacci", ["demo/fibonacci/fibonacci.pyx"]),
+    Extension("primes", ["demo/primes/primes.pyx"]),
+]
 
 setup(
     name="demo",
-    ext_modules=[
-        Extension("fibonacci", ["demo/fibonacci/fibonacci.pyx"]),
-        Extension("primes", ["demo/primes/primes.pyx"]),
-    ],
+    ext_modules=cythonize(extensions, annotate=True),
 )
